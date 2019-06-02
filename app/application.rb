98fa549bdccccb4e5bfc06322b71bad0058da364
Amy Cen <amy.cen@blackrock.com>
2019-06-02 06:25:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,12 @@
 class Application
-  resp = Rack::Response.new
-  if Time.now.hour <= 12
-    resp.write "Good Morning!"
-  else
-    resp.write "Good Afternoon!"
+  def call(env)
+
+    resp = Rack::Response.new
+    if Time.now.hour <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
   end
-  resp.finish
 end
